97a86887759bd070e2330471495a3be5651a7ef6
Fatman007 <alberthartanto94@gmail.com>
2018-07-01 21:22:04 +0700

    jatis

--- jatis_2.js
+++ jatis_2.js
@@ -1,3 +1,13 @@
+// const deret = (startNum, limit) => {
+//     let arr = []
+//     for (let i = 0; i < limit; i++) {
+//       arr.push(i === 0 ? startNum : arr[i - 1] + 3)
+//     }
+//     return arr
+//   }
+  
+//   console.log(deret(2, 10))
+
 function loop(awal) {
 
     while(true) {
--- jatis_4.js
+++ jatis_4.js
@@ -1,48 +1,48 @@
-var mysql = require("mysql");
-var express = require('express');
-var app = express();
+// var mysql = require("mysql");
+// var express = require('express');
+// var app = express();
 
-var date = new Date();
+// var date = new Date();
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-var bodyParser = require('body-parser')
-var url = bodyParser.urlencoded({ extended: false })
+// var bodyParser = require('body-parser')
+// var url = bodyParser.urlencoded({ extended: false })
 
-var connection = mysql.createConnection
-(
-    {
-        host: "127.0.0.1",
-        port: 3307,
-        database: "jatis_4",
-        user: "root",
-        password: "usbw",
-    }
-);
+// var connection = mysql.createConnection
+// (
+//     {
+//         host: "127.0.0.1",
+//         port: 3307,
+//         database: "jatis_4",
+//         user: "root",
+//         password: "usbw",
+//     }
+// );
 
 class Voucher {
     constructor() {
       this.listVoucher = []
     }
-    generate() { 
-      let l = this.listVoucher.length
-      this.listVoucher.push({
-        id: l === 0 ? 0 : this.listVoucher[l - 1].id + 1,
-        redeem: false
-      })
+    generate(amount) {
+      while (amount--) {
+        let l = this.listVoucher.length
+        this.listVoucher.push({
+          id: l === 0 ? 0 : this.listVoucher[l - 1].id + 1,
+          redeem: false
+        })
+      }
     }
     redeem(id) {
-      this.listVoucher = this.listVoucher.map(el => el.redeem === false && el.id === id ? { ...el, redeem: true } : el)
+      this.listVoucher = this.listVoucher.map(el => 
+      el.redeem === false && el.id === id ? { ...el, redeem: true } : el)
     }
   }
   
   let v = new Voucher
   
-  v.generate()
-  v.generate()
-  v.generate()
-  v.generate()
+  v.generate(10) // buat generate 10 voucher
   
-  v.redeem(1)
+  v.redeem(1) // setiap di redeem, per id jadi parameter, statusnya dari false jadi true
   v.redeem(3)
-  console.log(v.listVoucher[0])
+  console.log(v.listVoucher)
